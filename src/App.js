f0df5827264015440a10aa4b4d3d5189c14b088b
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-08-03 17:56:56 +0200

    Imported child components.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Search from './Search'
+import Map from './Map'
 
 class App extends Component {
   render() {
---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Details from './Details'
+
+export class Search extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Search
