d33d6d320c3fda6981045369cecce04915c3e7fb
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-08-04 23:33:34 +0200

    Map initialized.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
-import Search from "./Search";
-import Map from "./Map";
 import logo2 from "./images/logo2.png";
-
-class App extends Component {
+import MapContainer from './MapContainer'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+export class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
           <a href="#" id="menu">
             <svg width="30" height="30">
-              <path d="M0,5 30,5" stroke="#fff" stroke-width="5" />
-              <path d="M0,14 30,14" stroke="#fff" stroke-width="5" />
-              <path d="M0,23 30,23" stroke="#fff" stroke-width="5" />
+              <path d="M0,5 30,5" stroke="#fff" strokeWidth="5" />
+              <path d="M0,14 30,14" stroke="#fff" strokeWidth="5" />
+              <path d="M0,23 30,23" stroke="#fff" strokeWidth="5" />
             </svg>
           </a>
           <img id="logo" src={logo2} alt="home" />
         </header>
+        <MapContainer />
       </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/MapContainer.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map id='map' google={this.props.google} zoom={14} initialCenter={{
+        lat: 47.4725185,
+        lng: 18.866602
+      }}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyDwcyyo7poh2Jp3dnOEyP9HdRK2CKKcRXQ")
+})(MapContainer)
