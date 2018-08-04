2c68d1bdadb0d02b603c9358fd17412a79ae1f8f
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-08-05 00:13:34 +0200

    Defined map propTypes and default props.

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,29 +1,44 @@
 import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
 
 export class MapContainer extends Component {
   render() {
     return (
-      <Map id='map' google={this.props.google} zoom={14} initialCenter={{
-        lat: 47.4725185,
-        lng: 18.866602
-      }} onClick={this.onMapClicked}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
+      <Map
+        id="map"
+        google={this.props.google}
+        zoom={14}
+        initialCenter={{
+          lat: 47.4725185,
+          lng: 18.866602
+        }}
+        onClick={this.onMapClicked}
+      >
+        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
         <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-            </div>
+          <div />
         </InfoWindow>
       </Map>
     );
   }
 }
 
+Map.propTypes = {
+  google: React.PropTypes.object,
+  zoom: React.PropTypes.number,
+  initialCenter: React.PropTypes.object
+};
+
+Map.defaultProps = {
+  zoom: 14,
+  initialCenter: {
+    lat: 47.4725185,
+    lng: 18.866602
+  }
+};
 
 export default GoogleApiWrapper({
-  apiKey: ("AIzaSyDwcyyo7poh2Jp3dnOEyP9HdRK2CKKcRXQ")
-})(MapContainer)
+  apiKey: "AIzaSyDwcyyo7poh2Jp3dnOEyP9HdRK2CKKcRXQ"
+})(MapContainer);
